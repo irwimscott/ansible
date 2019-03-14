5d773ebe6f960018f1d845501c1456abffa92573
irwimscott <irwimscott@gmail.com>
2019-03-14 07:12:18 -0300

    Create api.R

---
+++ api.R
@@ -0,0 +1,32 @@
+# plumber.R
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg=""){
+  list(msg = paste0("The message is: '", msg, "'"))
+}
+
+#* Plot a histogram
+#* @png
+#* @get /plot
+function(){
+  rand <- rnorm(100)
+  hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b){
+  as.numeric(a) + as.numeric(b)
+}
+
+#* My test selic
+#* @serializer unboxedJSON
+#* @get /calcpu
+function(){
+  total = as.numeric(1500) * as.numeric(100)
+  list(result=total)
+}
